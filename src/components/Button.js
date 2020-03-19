343f7b831d9831a8d518390ff94a495b6da2994e
Lucas <cekanlucas@gmail.com>
2020-03-19 18:25:45 -0400

    button components render for confirm and danger

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,20 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return <button className={buttonClass}>{props.children}</button>;
 }
+
+{/*
+<Button danger>Cancel</Button>
+<button className="button button--confirm">Confirm</button>
+
+*/} 
