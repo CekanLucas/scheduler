60eabe5a214a9059329338807001b187c9698b5a
Lucas <cekanlucas@gmail.com>
2020-03-21 21:03:45 -0400

    DayListItem component renders and tests as expected

---
+++ src/components/DayListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const formatSpots = (spot) => {
+    return spot === 0 ? 'no spots remaining' : 
+      spot === 1 ? '1 spot remaining' : `${spot} spots remaining`
+  };
+  const dayListStyle = "day-list__item " + classnames({
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayListStyle}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,7 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem";
 
 storiesOf("Button", module)
   .addParameters({
@@ -21,4 +22,15 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
